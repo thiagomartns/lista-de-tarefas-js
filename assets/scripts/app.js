300750fcb689e7dbf9ad6789df705f973d017c42
Thiago <tmg3.mn@gmail.com>
2022-08-09 13:00:02 -0300

    innit

---
+++ assets/scripts/app.js
@@ -0,0 +1,56 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const todo = addForm.add.value.trim();
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+})
+
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
+
+const filterTodos = term => {
+  Array.from(list.children)
+    .filter(listChild => {
+      return !listChild.textContent.includes(term);
+    })
+    .forEach(listChild => {
+      listChild.classList.add('filtered');
+    });
+
+  Array.from(list.children)
+    .filter(listChild => {
+      return listChild.textContent.includes(term);
+    })
+    .forEach(listChild => {
+      listChild.classList.remove('filtered');
+    });
+};
+
+const generateTemplate = todo => {
+
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+
+  list.innerHTML += html;
+
+};
